ad756376938967f785390189776f8aa90884cb21
Ajit Dingankar <Ajit.Dingankar@ieee.org>
2016-07-05 00:04:52 -0700

    Completed code and tested with 3 matrices in cachematrix_test.R

---
+++ cachematrix_test.R
@@ -0,0 +1,62 @@
+source('cachematrix.R')
+
+cat("####\nSimple 2x2 matrix\n")
+cat("makeCacheMatrix(matrix(1:4, 2, 2))\n")
+m <- makeCacheMatrix(matrix(1:4, 2, 2))
+
+cat("m$get()\n")
+print(m$get())
+
+cat("Initially the cached inverse is NULL\n")
+cat("m$getinv()\n")
+print(m$getinv())
+
+cat("First cacheSolve(m)\n")
+print(cacheSolve(m))
+cat("cacheSolve(m) again\n")
+print(cacheSolve(m))
+cat("cacheSolve(m) third time\n")
+print(cacheSolve(m))
+
+####
+cat("\n####\nRandom 2x2 matrix\n")
+cat("m$set(matrix(rnorm(4), 2, 2))\n")
+m$set(matrix(rnorm(4), 2, 2))
+
+cat("m$get()\n")
+print(m$get())
+
+cat("Check that after 'set' cached inverse is NULL\n")
+cat("m$getinv()\n")
+print(m$getinv())
+
+cat("First cacheSolve(m)\n")
+print(cacheSolve(m))
+cat("cacheSolve(m) again\n")
+print(cacheSolve(m))
+cat("cacheSolve(m) third time\n")
+print(cacheSolve(m))
+
+####
+cat("\n####\nRandom 16x16 matrix\n")
+cat("m$set(matrix(rnorm(256), 16, 16))\n")
+m$set(matrix(rnorm(256), 16, 16))
+
+cat("m$get()\n")
+print(m$get())
+
+cat("Check that after 'set' cached inverse is NULL\n")
+cat("m$getinv()\n")
+print(m$getinv())
+
+start.time <- Sys.time()
+cacheSolve(m)
+end.time <- Sys.time()
+cat("Time taken for the first cacheSolve(m)", end.time - start.time, "\n")
+
+start.time <- Sys.time()
+cacheSolve(m)
+end.time <- Sys.time()
+cat("Time taken for the second cacheSolve(m)", end.time - start.time, "\n")
+
+####
